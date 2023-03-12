da87630492e45ad38c84c2cbc197c2d393334827
JohnnyIrish145 <127519322+JohnnyIrish145@users.noreply.github.com>
2023-03-12 11:58:52 +0200

    Add files via upload

---
+++ Liēprūlavąno Dictionary_1.0 release.py
@@ -0,0 +1,191 @@
+Error = "Sorry, but we currently do not have that word"
+Design = "--------------------------------------------------"
+DesignEn = "English:"
+DesignLp = "Liēprūlavąno:"
+DesignChange = 'If you would like to change the mode, type "change" or "break"!'
+print(Design)
+print("------Welcome to the Liēprūlavąno dictionary------")
+print(Design)
+print("Made by JohnnyIrish#3587!")
+Wordlist=["Ten","Dėzimt","10","Dėzimt","Three","Trīs","3","Trīs","Four","Čioturi","4","Čioturi","Five","Pènki","5","Pènki","Six","Šešjai","6","Šešjai","Seven","Sepci","7","Sepci","Eight","Eistuoni","8","Eistuoni","Nine","Tevīni","9","Tevīni","Eleven","Aïèns","11","Aïèns","Twelve","Dvėïc","12","Dvėïc","Thirteen","Tryrikö","13","Tryrikö","Fourteen","Čioturikö","14","Čioturikö","Fifteen","Piēnrikö","15","Piēnrikö","Sixteen","Šešjarikö","16","Šešjarikö","Seventeen","Seprikö","17","Seprikö","Eighteen","Eistuorikö","18","Eistuorikö","Nineteen","Tèvirikö","19","Tèvirikö","Two","Duï","2","Duï","One","Aīns","1","Aīns","Zero","Nulas","0","Nulas","Twenty","Dvèzimt","20","Dvèzimt","Thirty","Tryzimt","30","Tryzimt","Forty","Čiotuzimt","40","Čiotuzimt","Fifty","Pènkzimt","50","Pènkzimt","Sixty","Šešzimt","60","Šešzimt","Seventy","Sepzimt","70","Sepzimt","Eighty","Eistuozimt","80","Eistuozimt","Ninety","Tëzimt","90","Tëzimt","Hundred","Šīïmtes","100","Šīïmtes","Thousand","Tūktstins","1000","Tūktstins","Milion","Miljöns","1000000","Milion","Berry","Uogė","Berries","Uogė","Moss","Samkasai","Mushroom","Brīgas","Mushrooms","Brīgas","Žyla","Bushes","Flower","Gōluos","Flowers","Gōluos","Grass","Žyla","Bushes","Žommedzis","Aspen","Kratso","Linden","Lipė","Pine","Pūskä","Maple","Eklas","Ash","Pelnys","Ashes","Pelnys","Birch","Pircukas","Spruce","Ègla","Oak","Ąžōlas","Branches","Šaksės","Tree","Medzis","Plant","Žalynkasai","Plants","Žalynkasai","Sweet","Madrus","Fruit","Valsas","Juice","Falatas","Cucumber","Āgurkis","Tomato","Tomïdoris","Salad","Žālonai","Meat","Rèblas","Sandwich","Rèbladounis","Water","Voundo","Bread","Dounis","Baguette","Bäguotë","Ball","Rola","Circle","Valus","Triangle","Trīsikampas","Rectangle","Nėlgasskvaras","Square","Skvaras","Tall","Kėkštas","Height","Žömas","Short","Tirumpas","Long","Nėlgas","Corner","Kampas","Angle","Kumpis","Island","Silsa","Alive","Gīvincinis","Proudly","Dèngi","Proud","Dèngi","Motherland","Mīmrankštis","Region","Rankštis","All","Viskuo","Full","Pilns","Fills","Pilns","Fill","Pilns","Visible","Mancitim","Moon","Menuolis","Sun","Sonsa","Night","Nakt","Evening","Vėkes","Day","Deīna","Days","Deīna","Morning","Rètas","Mornin","Rètas","Progress","Progręsis","Progression","Progręsis","Someone","Kadjes","Someones","Kadjes","Simple","Papkāršè","Word","Zuodis","Phrase","Frūzė","Here","Čiōnai","There","Ten","Are","Osi","Is","Uzra","Name","Vardilis","Names","Vardilis","When","Tada","Which","Kūris","For","Dzun","Behind","Dzun","Everyone","Vesi","Stand","Tulint","Stood","Tulint","Work","Dirbint","Worked","Dirbint","Works","Dirbint","Wake","Kelint","Awake","Kelint","Woke","Kelint","Rise","Kylint","Finish","Beigt","Mean","Pacgit","Add","Dėt","Continue","Türptint","Die","Mirėt","Dead","Mirėt","Laugh","Jökt","Listen","Klausiet","Lazy","Tengytau","Hit","Trönkt","Drink","Gèrt","Drank","Gèrt","Drunk","Gèrt","Choke","Ledzert","Convert","Vest","Kill","Skèrt","Know","Žiñot","Known","Žiñot","Knew","Žiñot","Love","Muōlit","Strangle","Nožnègöt","Shit","Šèikt","Cry","Grèžt","Scream","Rökt","Sing","Dainout","Sang","Dainout","Sung","Dainout","Read","Skytit","Hide","Slīpt","Shower","Prōstis","Sleep","Mygt","Eat","Vöst","Teach","Smōktint","Learn","Ismōkt","Make","Gamēnt","Create","Kort","Like","Pantikt","Be","Būt","Have","Torėt","Young","Jouns","Old","Sēns","Teenager","Sēnsveiñtys","Kid","Veiñtys","Child","Veiñtys","Baby","Vėšlīs","Close","Šōla","Wall","Ūna","Joke","Juöks","How","Kāipgi","Good","Labs","Simple","Papkāršie","Here","Čiōnai","There","Ten","Who","Kās","With","So","Into","Ė","And","Ur","Nouns","Daikszuodis","Want","Nogribt","See","Macyt","Speak","Bīlint","Walk","Ēint","Go","Ēint","Verbs","Vyksmozuodisai","Bank","Bonkas","Forest","Mėšks","Village","Cèmañas","School","Mōkenka","Bridge","Upkels","Apartment","Duïgtrobė","House","Trobė","City","Męstas","Locations","Viētas","Shoes","Kliūsau","T-Shirt","Mike","Pants","Antköjs","Skirt","Šlēbkojs","Dress","Šlęba","Hat","Šupkèra","Clothing","Antkūns","Boat","Kūgis","Bus","Autobus","Door","Dūrats","Car","Automobils","Plane","Skrīstmašima","Train","Vilkęñas","Transportation","Transportis","Horse","Zirks","Mouse","Pylė","Pig","Cūka","Cow","Kūrvė","Bird","Pètnikas","Fish","Zevis","Cat","Kātè","Dog","Šuo","Animals","Gyvenkasai","Gray","Pïlka","Black","Jouda","Pink","Rūza","Balta","Baltina","Purple","Violinė","Brown","Brūna","Blue","Melūna","Orange","Oranžė","Yellow","Dželtona","Green","Žālia","Red","Raīdouna","Colour","Spėlvas","Choke","Ledzertik","Astronaut","Kosmosis","Alkocholic","Pijöks","Idiot","Īdījötas","Cop","Kopsas","Is","Īr","My","Aęsas","Where","Kvër","Live","Dzeviñti","Know","Žïñotau","Loves","Muōliti","Strangle","Nožnègsötisijau","You","Tebbā","Shit","Šèikt","Go","Ēintik","Nein","🥚 NEIN NEIN NEIN 🥚","No","Ñe","Dude","Veci","Dude","Vėcyk","Nope","Ñe","Numbers","Bīkasai","Son","Sūns","Daughter","Dōukta","Human","Zmaugīs","Aunt","Töta","Grandpa","Vecèkas","Uncle","Dèdąs","Sister","Sėstrė","Father","Bātkys","Dad","Tėvs","Grandma","Bābolė","Bro","Brōdas","Brother","Brōdas","Mother","Mötiña","Mummy","Mīma","Mammy","Mīma","Mommy","Mīma","Mum","Mīma","Mam","Mīma","Mom","Mīma","Boy","Bīrs","Girl","Pañölė","Woman","Pañölė","Man","Bīrs","She","Jë","Him","Jön","Myself","Sūve","They","Jëtt","We","Mēs","I","Aęs","Me","Aęs","Rule","Taisikė","People","Asmīñsai","Predictive","Tarkinė","Telling","Lèpënė","Form","Šypa","Direct","Tèsignė","Forms","Šypas","Future","Būïvsims","Repeating","Kartojs","Past","Būïvs","Time","Leīkas","Current","Daba","Times","Leīkasai","Adjectives","Budazuodisaipā","Nouns","Daikszuodisaipā","Verbs","Vyksmozuodisaipā","Vestim","Converting","Case","Leñknīs","Words","Zuodis","Plurals","DuÏgīskīta","With","Som","Soft","Mīkstos","Hard","Kècos","Vovels","Bėlsos","Long","Èlgös","Cases","Leñksnīsai","Locative","Vietkurjëtinis","Instrumental","Sukuotebesinis","Accusative","Kurįginis","Dative","Kamginis","Genetive","Kurioginis","Vocative","Kvietinis","Nominative","Verdinis","You","Tebb","How","Kypgi","Please","Lüdz","Night","Naktin","Evening","Vėkes","Afternoon","Deīna","Morning","Rėtas","Mornin","Rėtas","Thank","Dèmkön","No","Ñe","Yes","Typ","Yeah","Typ","Phrases","Frūzės","Goodbye","Nątc","Bye","Nątc","Hello", "Ladès", "Hey", "Ladès","Hi", "Ladès"]
+MaxAmount = str(Wordlist)
+MaxAmount = len(Wordlist)
+MaxAmount = MaxAmount - 2
+
+while True:
+    print("")
+    print("Translation mode selection:")
+    print('type "en" if you want to translate from english to liēprūlavąno')
+    print('type "lp" if you want to translate from liēprūlavąno to english')
+    console = input()
+    console = console.lower()
+    if console == "lp":
+        print("")
+        print("Type a word in liēprūlavąno below to translate it to english.")
+        print(DesignChange)
+        print("")
+        while True:
+            print(DesignLp)
+            console = input()
+            ChangeMode = console.lower()
+            if ChangeMode == "change":
+                break
+            if ChangeMode == "break":
+                break
+            ConsoleWords = console.split()
+            ConsolePlace = 0
+            ConsoleLength = len(ConsoleWords)
+            if console == "":
+                print("Please type something else")
+                print("")
+                print(Design)
+            elif console.isspace() == True:
+                print("Please type something else")
+                print("")
+                print(Design)
+            else:
+                Finale = ""
+                Finale = str(Finale)
+                print(DesignEn)
+                ConsolePlace2 = 0
+                while ConsoleLength>ConsolePlace2:
+                    ConsoleAll = str(ConsoleWords[ConsolePlace])
+                    ConsolePlace2 = ConsolePlace + 1
+                    Letters = [str(Letters) for Letters in ConsoleAll]
+                    First_letter = str(Letters[0])
+                    ConsolePlace += 1
+                    WordPlace = 1
+                    ConsoleLower = ConsoleAll.lower()
+                    if First_letter.isnumeric():
+                        while True:
+                            EnWord = Wordlist[WordPlace]
+                            EnWord = str(EnWord).lower()
+                            if ConsoleLower == EnWord:
+                                WordPlace = WordPlace - 1
+                                FinaleLpWord = Wordlist[WordPlace].lower()
+                                Finale = Finale + FinaleLpWord + " "
+                                WordPlace = 0
+                                break
+                            elif WordPlace == MaxAmount - 1:
+                                Finale = Finale + ConsoleAll + " "
+                                break
+                            else:
+                                WordPlace += 2
+                    elif First_letter.isupper():
+                        while True:
+                            EnWord = Wordlist[WordPlace]
+                            EnWord = str(EnWord).lower()
+                            if ConsoleLower == EnWord:
+                                WordPlace = WordPlace - 1
+                                FinaleLpWord = Wordlist[WordPlace]
+                                Finale = Finale + FinaleLpWord + " "
+                                WordPlace = 0
+                                break
+                            elif WordPlace == MaxAmount - 1:
+                                Finale = Finale + ConsoleAll + " "
+                                break
+                            else:
+                                WordPlace += 2
+                    elif First_letter.islower():
+                        while True:
+                            EnWord = Wordlist[WordPlace]
+                            EnWord = str(EnWord).lower()
+                            if ConsoleLower == EnWord:
+                                WordPlace = WordPlace - 1
+                                FinaleLpWord = Wordlist[WordPlace].lower()
+                                Finale = Finale + FinaleLpWord + " "
+                                WordPlace = 0
+                                break
+                            elif WordPlace == MaxAmount - 1:
+                                Finale = Finale + ConsoleAll + " "
+                                break
+                            else:
+                                WordPlace += 2
+                else:
+                    print(Finale)
+                    print(Design)
+    elif console == "en":
+        print("")
+        print("Type a word in english below to translate it to liēprūlavąno.")
+        print(DesignChange)
+        print("")
+        while True:
+            print(DesignEn)
+            console = input()
+            ChangeMode = console.lower()
+            if ChangeMode == "change":
+                break
+            if ChangeMode == "break":
+                break
+            ConsoleWords = console.split()
+            ConsolePlace = 0
+            ConsoleLength = len(ConsoleWords)
+            if console == "":
+                print("Please type something else")
+                print("")
+                print(Design)
+            elif console.isspace() == True:
+                print("Please type something else")
+                print("")
+                print(Design)
+            else:
+                Finale = ""
+                Finale = str(Finale)
+                print(DesignLp)
+                ConsolePlace2 = 0
+                while ConsoleLength>ConsolePlace2:
+                    ConsoleAll = str(ConsoleWords[ConsolePlace])
+                    ConsolePlace2 = ConsolePlace + 1
+                    Letters = [str(Letters) for Letters in ConsoleAll]
+                    First_letter = str(Letters[0])
+                    ConsolePlace += 1
+                    WordPlace = 0
+                    ConsoleLower = ConsoleAll.lower()
+                    if First_letter.isnumeric():
+                        while True:
+                            EnWord = Wordlist[WordPlace]
+                            EnWord = str(EnWord).lower()
+                            if ConsoleLower == EnWord:
+                                WordPlace = WordPlace + 1
+                                FinaleLpWord = Wordlist[WordPlace].lower()
+                                Finale = Finale + FinaleLpWord + " "
+                                WordPlace = 0
+                                break
+                            elif WordPlace == MaxAmount:
+                                Finale = Finale + ConsoleAll + " "
+                                break
+                            else:
+                                WordPlace += 2
+                    elif First_letter.isupper():
+                        while True:
+                            EnWord = Wordlist[WordPlace]
+                            EnWord = str(EnWord).lower()
+                            if ConsoleLower == EnWord:
+                                WordPlace = WordPlace + 1
+                                FinaleLpWord = Wordlist[WordPlace]
+                                Finale = Finale + FinaleLpWord + " "
+                                WordPlace = 0
+                                break
+                            elif WordPlace == MaxAmount:
+                                Finale = Finale + ConsoleAll + " "
+                                break
+                            else:
+                                WordPlace += 2
+                    elif First_letter.islower():
+                        while True:
+                            EnWord = Wordlist[WordPlace]
+                            EnWord = str(EnWord).lower()
+                            if ConsoleLower == EnWord:
+                                WordPlace = WordPlace + 1
+                                FinaleLpWord = Wordlist[WordPlace].lower()
+                                Finale = Finale + FinaleLpWord + " "
+                                WordPlace = 0
+                                break
+                            elif WordPlace == MaxAmount:
+                                Finale = Finale + ConsoleAll + " "
+                                break
+                            else:
+                                WordPlace += 2
+                else:
+                    print(Finale)
+                    print(Design)
